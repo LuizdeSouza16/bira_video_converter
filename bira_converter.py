5476d14db06aecc5a0f8f35f35bc2a53825f7fc0
LuizdeSouza16 <luizdesouza16@gmail.com>
2023-03-10 17:15:33 -0300

    feat: Added support for converting only one file

--- bira_converter.py
+++ bira_converter.py
@@ -9,25 +9,48 @@ else:
 
 # paramters for ffmpeg
 codec_video = '-c:v libx264'
-crf = '-crf 20'
+crf = '-crf 24'
 preset = '-preset medium'
 code_audio = '-c:a aac'
 bitrate_audio = '-b:a 128k'
 
-path_input = r'.\\videos'
-path_output = r'.\\output_videos'
+path_input = r'.\videos'
+path_output = r'.\output_videos'
+parameter = sys.argv
 
-for root_files, folders, files in os.walk(path_input):
-    for file in files:
-        if not fnmatch.fnmatch(file, '*.avi'):
-            continue
-        
-        path_file = os.path.join(root_files, file)
-        file_name, file_extension = os.path.splitext(file)
-        
-        output_file = f'{path_output}\\{file_name}.mp4'
-        
-        command = f'{command_ffmpeg} -i "{path_file}" {codec_video} {crf} {preset} {code_audio}  '\
-            f'{bitrate_audio} "{output_file}"'
-        
-        os.system(command)
+def recursiveConversion(path_input, path_output):
+    for root_files, folders, files in os.walk(path_input):
+        for file in files:
+            if not fnmatch.fnmatch(file, '*.avi'):
+                continue
+            
+            path_file = os.path.join(root_files, file)
+            file_name, file_extension = os.path.splitext(file)
+            
+            output_file = f'{path_output}\\{file_name}.mp4'
+            
+            command = f'{command_ffmpeg} -i "{path_file}" {codec_video} {crf} {preset} {code_audio}  '\
+                f'{bitrate_audio} "{output_file}"'
+            
+            os.system(command)
+
+def fileConversion(path_input, path_output, video):
+        for root_files, folders, files in os.walk(path_input):
+            if not fnmatch.fnmatch(video, '*.avi'):
+                continue
+            
+            path_file = f'{path_input}\\{video}'
+            file_name, file_extension = os.path.splitext(video)
+            
+            output_file = f'{path_output}\\{file_name}.mp4'
+            
+            command = f'{command_ffmpeg} -i "{path_file}" {codec_video} {crf} {preset} {code_audio}  '\
+                f'{bitrate_audio} "{output_file}"'
+            
+            os.system(command)
+            
+
+if len(parameter) == 1:
+    recursiveConversion(path_input, path_output)
+else:
+    fileConversion(path_input, path_output, parameter[1])
